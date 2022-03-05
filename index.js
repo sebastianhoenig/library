a313896293672fd191ed0a8a6de29dd8f485ff53
Sebastian Hönig <hoenigsebastian@gmail.com>
2022-03-05 10:49:03 +0100

    added form validation

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 let myLibrary = [];
 let libraryVisual = document.getElementsByClassName("library")[0];
 let cards = document.getElementsByClassName("card");
-let modalTitle = document.getElementById("title");
-let modalAuthor = document.getElementById("author");
-let modalPages = document.getElementById("pages");
+const modalTitle = document.getElementById("title");
+const modalAuthor = document.getElementById("author");
+const modalPages = document.getElementById("pages");
+
+const modalTitleError = document.querySelector("#title + span.error");
+const modalAuthorError = document.querySelector("#author + span.error");
+const modalPagesError = document.querySelector("#pages + span.error");
+
 let modalRead = document.getElementById("read");
 let submitButton = document.getElementsByClassName("form")[0];
 const ADDBOOK = document.getElementById("add");
-const MODAL = document.getElementsByClassName("modal")[0]; //LOOK AT THSI
+const MODAL = document.getElementsByClassName("modal")[0];
 const OVERLAY = document.getElementById("overlay");
 let deleteButtons = document.getElementsByClassName("delete-book");
 
@@ -94,8 +99,48 @@ function getTarget(e) {
   return e.target || e.srcElement;
 }
 
+function showError() {
+  if (!modalTitle.validity.valid) {
+    modalTitleError.textContent = "Not a valid title";
+    modalTitleError.className = "error active";
+  }
+  if (!modalAuthor.validity.valid) {
+    modalAuthorError.textContent = "Not a valid author";
+    modalAuthorError.className = "error active";
+  }
+  if (!modalPages.validity.valid) {
+    modalPagesError.textContent = "Not a valid number";
+    modalPagesError.className = "error active";
+  }
+}
+
+function resetErrorMessages() {
+  modalTitleError.textContent = "";
+  modalAuthorError.textContent = "";
+  modalPagesError.textContent = "";
+  modalTitleError.className = "error";
+  modalAuthorError.className = "error";
+  modalPagesError.className = "error";
+}
+
+modalTitle.addEventListener("focus", (event) => {
+  modalTitleError.textContent = "";
+  modalTitleError.className = "error";
+});
+
+modalAuthor.addEventListener("focus", (event) => {
+  modalAuthorError.textContent = "";
+  modalAuthorError.className = "error";
+});
+
+modalPages.addEventListener("focus", (event) => {
+  modalPagesError.textContent = "";
+  modalPagesError.className = "error";
+});
+
 submitButton.addEventListener("submit", function (event) {
   if (modalTitle.value && modalAuthor.value && modalPages.value) {
+    resetErrorMessages();
     addBookToLibrary(
       modalTitle.value,
       modalAuthor.value,
@@ -106,6 +151,10 @@ submitButton.addEventListener("submit", function (event) {
     MODAL.classList.remove("active");
     OVERLAY.classList.remove("active");
     submitButton.reset();
+  } else {
+    resetErrorMessages();
+    showError();
+    event.preventDefault();
   }
 });
 
@@ -120,6 +169,7 @@ ADDBOOK.addEventListener("click", () => {
 });
 
 OVERLAY.addEventListener("click", () => {
+  resetErrorMessages();
   MODAL.classList.remove("active");
   OVERLAY.classList.remove("active");
   submitButton.reset();
